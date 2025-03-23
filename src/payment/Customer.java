5e26eddca33928d6db2ab01cd42d118d62b5dbdd
fati <fatemesadatsedigh@gmail.com>
2025-03-23 11:24:46 +0330

    commit-writing-Customer

--- src/payment/Customer.java
+++ src/payment/Customer.java
@@ -1,4 +1,28 @@
 package payment;
 
-public class Customer {
+import java.util.ArrayList;
+
+public abstract class Customer {
+    protected String name;
+    protected ArrayList<String> paymentHistory;
+
+    public Customer(String name) {
+        this.name = name;
+        this.paymentHistory = new ArrayList<>();
+    }
+
+    public abstract void displayCustomerInfo();
+
+    public void makePayment(PaymentStrategy paymentStrategy, double amount) {
+        paymentStrategy.pay(amount);
+        String paymentDetails = "Payment: " + amount + " - " + paymentStrategy.getPaymentDetails();
+        paymentHistory.add(paymentDetails);
+    }
+
+    public void showPaymentHistory() {
+        System.out.println("Payment history for " + name + ":");
+        for (String payment : paymentHistory) {
+            System.out.println(payment);
+        }
+    }
 }
